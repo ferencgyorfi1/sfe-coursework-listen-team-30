613106e907fe6edc96b400762e03fb5da4955346
ferencgyorfi1 <166602909+ferencgyorfi1@users.noreply.github.com>
2024-04-11 16:24:21 +0000

    React + Firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,60 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDl275GejcwkVYn6ISWvmejxROYmr_nrF8",
+  authDomain: "listen-database-5ec9c.firebaseapp.com",
+  projectId: "listen-database-5ec9c",
+  storageBucket: "listen-database-5ec9c.appspot.com",
+  messagingSenderId: "973415679044",
+  appId: "1:973415679044:web:15116ae9875c38e8f1a811",
+  measurementId: "G-BRFVXNRE5X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up successfully
+        const user = userCredential.user;
+        console.log("User registered successfully:", user);
+        return user;
+      })
+      .catch((error) => {
+        // Handle errors
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Registration error:", errorMessage);
+        throw new Error(errorMessage);
+      });
+  };
+
+  const login = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in successfully
+        const user = userCredential.user;
+        console.log("User logged in successfully:", user);
+        return user;
+      })
+      .catch((error) => {
+        // Handle errors
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error("Login error:", errorMessage);
+        throw new Error(errorMessage);
+      });
+  };
+  
+  export { auth, register, login };
